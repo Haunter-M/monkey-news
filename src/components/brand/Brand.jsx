9befec635354a2411f78bdbc603144606c92e2e6
Haunter-M <119825575+Haunter-M@users.noreply.github.com>
2022-12-05 01:40:12 -0500

    first commit

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './brand.css'; 
 import { google, slack, atlassian, dropbox, shopify } from './imports' ;
 
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Feature from '../../components/feature/Feature';
 import './features.css';
 
